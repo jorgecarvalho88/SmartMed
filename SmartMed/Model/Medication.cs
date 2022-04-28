6e69d87a0adc809b0ff3e75ccc801913cfa781fe
Jorge Carvalho <jorgecarvalho88@gmail.com>
2022-04-28 22:33:02 +0100

    Medication Model

---
+++ SmartMed/Model/Medication.cs
@@ -0,0 +1,47 @@
+﻿using DataExtension;
+using Validations;
+
+namespace SmartMed.Model
+{
+    public class Medication : BaseEntity
+    {
+        public Medication()
+        { }
+
+        public Medication(string name, int quantity)
+        {
+            SetName(name);
+            SetQuantity(quantity);
+        }
+
+        public string Name { get; protected set; }
+        public int Quantity { get; protected set; }
+        public DateTime CreationDate { get; protected set; }
+
+        #region Métodos
+
+        public void SetName(string name)
+        {
+            ValidateIsNullOrWhiteSpace(name, "Name");
+            Name = name;
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            ValidateQuantity(quantity);
+            Quantity = quantity;
+        }
+
+        private void ValidateIsNullOrWhiteSpace(string value, string property)
+        {
+            var error = StringValidator.ValidateIsNullOrWhiteSpace(value, property);
+            if (error != null) this.Errors.Add(error);
+        }
+
+        private void ValidateQuantity(int quantity)
+        {
+            if (quantity <= 0) this.Errors.Add("Quantity must by greater than 0");
+        }
+        #endregion
+    }
+}
---
+++ Validations/StringValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validations
+{
+    public static class StringValidator
+    {
+        public static string? ValidateIsNullOrWhiteSpace(string? value, string? property)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return property + ": Required Field";
+            }
+
+            return null;
+        }
+    }
+}
